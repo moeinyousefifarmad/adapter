714cd4df41c7f368c6a6f0a3da74df9bc1505dc6
moein yousefi farmad <indievyse@gmail.com>
2023-09-27 00:00:00 -0700

    player controller added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private float groundCheckRayDistance;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb2d;
+
+    private void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        if (IsOnGround() && Input.GetButtonDown("Jump")) Jump();
     }
+    private void Move()
+    {
+        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed , rb2d.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb2d.velocity = new Vector2(rb2d.velocity.x , jumpPower);
+    }
+
+    private bool IsOnGround() => Physics2D.Raycast(groundCheckPos.position , Vector2.down , groundCheckRayDistance , groundLayer);
 }
